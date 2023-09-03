bba98cbe55fa99db60b0569497665c7c9e55c183
neithanmo <neithanmo@gmail.com>
2023-09-02 20:40:12 -0600

    feat(benchmark) Measure time to get blocks and improvements

--- benches/save_blocks_bench.rs
+++ benches/save_blocks_bench.rs
@@ -1,110 +1,41 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 
-use namada_prototype::{utils::load_checksums, Database, Settings};
-use sqlx::postgres::PgPoolOptions;
-use sqlx::query;
-use sqlx::PgPool;
-use std::collections::HashMap;
-use std::fs;
-use std::time::Duration;
-use tendermint::block::Block;
+use namada_prototype::utils::load_checksums;
 use tendermint::block::Height;
+
+mod utils;
 
 const DATABASE_NAME: &str = "bench_db";
 
-async fn create_db(pool: &PgPool, name: &str) {
-    // now create bench database
-    let db_query = format!("CREATE DATABASE {}", name);
-
-    query(&db_query)
-        .execute(pool)
-        .await
-        .expect("Could not create database for benchmarks");
-}
-
-async fn destroy_db(pool: &PgPool, name: &str) {
-    let db_query = format!("DROP DATABASE {}", name);
-
-    query(&db_query)
-        .execute(pool)
-        .await
-        .expect("Could not create database for benchmarks");
-}
-
-async fn create_bench_database(pg_pool: &PgPool) -> Database {
-    let config = Settings::new().unwrap();
-    let config = config.database_config();
-
-    // lets connect to our default database, so from there
-    // we create another database that is going to be used for
-    // benches.
-    create_db(pg_pool, DATABASE_NAME).await;
-
-    // Now connect to the just created db
-    let config = format!(
-        "postgres://{}:{}@{}/{}",
-        config.user, config.password, config.host, DATABASE_NAME,
-    );
-
-    let pool = PgPoolOptions::new()
-        .max_connections(10)
-        .acquire_timeout(Duration::from_secs(30))
-        .connect(&config)
-        .await
-        .expect("Could not connect to bench database");
-
-    Database::with_pool(pool)
-}
-
-async fn save_blocks_bench(
-    db: &Database,
-    blocks: impl Iterator<Item = &mut Block>,
-    checksums: &HashMap<String, String>,
-) {
-    for block in blocks {
-        // we need to update the block height
-        // to avoid collisions between bench runs
-        db.save_block(&block, &checksums).await.unwrap();
-    }
-}
-
 fn configure_criterion() -> Criterion {
-    Criterion::default()
-        .sample_size(100) // For example, set sample size
-        .noise_threshold(0.05) // 5% noise threshold
+    Criterion::default().sample_size(10).noise_threshold(0.05) // 5% noise threshold
 }
 
-fn criterion_benchmark(c: &mut Criterion) {
-    let config = Settings::new().unwrap();
-    let config = config.database_config();
-
-    let checksums_map = load_checksums().unwrap();
-
-    let data = fs::read_to_string("./tests/blocks_vector.json").unwrap();
-    let mut blocks: Vec<Block> = serde_json::from_str(&data).unwrap();
-
-    let rt = tokio::runtime::Runtime::new().expect("could not create runtime");
-
-    // this database is used to connect to our default database which is used by tests.
-    // but we use it here to create an alternative database to use for benches
-    let db = rt.block_on(async { Database::new(config).await.unwrap() });
-
-    // destroy bench database if it exists
-    rt.block_on(async {
-        destroy_db(db.pool(), DATABASE_NAME).await;
-    });
+fn save_blocks_benchmark(c: &mut Criterion) {
+    let rt = tokio::runtime::Runtime::new().unwrap();
 
     let bench_db = rt.block_on(async {
-        let db = create_bench_database(db.pool()).await;
+        // get helper database to create/destroy a new db used for this specific benchmark
+        let helper_db = utils::helper_db().await;
+
+        // destroy bench database if it exists
+        utils::destroy_bench_database(helper_db.pool(), DATABASE_NAME).await;
+
+        // a fresh state to ensure best benchmarking results
+        let db = utils::create_bench_database(helper_db.pool(), DATABASE_NAME).await;
 
         db.create_tables().await.unwrap();
         db
     });
 
-    let mut block_idx: u32 = 0;
+    let mut block_idx: u32 = 1;
+
+    // load testing data
+    let mut blocks = utils::load_blocks();
+    let checksums_map = load_checksums().unwrap();
 
     // start benchmarking here
-    c.bench_function("function_name", |b| {
+    c.bench_function("save_block", |b| {
         b.iter(|| {
             rt.block_on(async {
                 // this allows us to avoid collisions
@@ -115,16 +46,15 @@ fn criterion_benchmark(c: &mut Criterion) {
                     b
                 });
 
-                save_blocks_bench(&bench_db, iter, &checksums_map).await
+                utils::save_blocks(&bench_db, iter, &checksums_map).await
             });
         });
     });
 }
 
-// criterion_group!(benches, criterion_benchmark);
 criterion_group! {
-    name = benches;
+    name = save_blocks_benches;
     config = configure_criterion();
-    targets = criterion_benchmark
+    targets = save_blocks_benchmark
 }
-criterion_main!(benches);
+criterion_main!(save_blocks_benches);
---
+++ benches/utils.rs
@@ -0,0 +1,74 @@
+use namada_prototype::{Database, Settings};
+use sqlx::postgres::PgPoolOptions;
+use sqlx::query;
+use sqlx::PgPool;
+use std::collections::HashMap;
+use std::fs;
+use std::time::Duration;
+use tendermint::block::Block;
+
+async fn create_db(pool: &PgPool, name: &str) {
+    // now create bench database
+    let db_query = format!("CREATE DATABASE {}", name);
+
+    query(&db_query)
+        .execute(pool)
+        .await
+        .expect("Could not create database for benchmarks");
+}
+
+async fn destroy_db(pool: &PgPool, name: &str) {
+    let db_query = format!("DROP DATABASE {}", name);
+
+    _ = query(&db_query).execute(pool).await
+}
+
+pub async fn create_bench_database(pg_pool: &PgPool, name: &str) -> Database {
+    let config = Settings::new().unwrap();
+    let config = config.database_config();
+
+    // lets connect to our default database, so from there
+    // we create another database that is going to be used for
+    // benches.
+    create_db(pg_pool, name).await;
+
+    // Now connect to the just created db
+    let config = format!(
+        "postgres://{}:{}@{}/{}",
+        config.user, config.password, config.host, name,
+    );
+
+    let pool = PgPoolOptions::new()
+        .max_connections(10)
+        .acquire_timeout(Duration::from_secs(30))
+        .connect(&config)
+        .await
+        .expect("Could not connect to bench database");
+
+    Database::with_pool(pool)
+}
+
+pub async fn destroy_bench_database(pg_pool: &PgPool, name: &str) {
+    destroy_db(pg_pool, name).await
+}
+
+pub fn load_blocks() -> Vec<Block> {
+    let data = fs::read_to_string("./tests/blocks_vector.json").unwrap();
+    serde_json::from_str(&data).unwrap()
+}
+
+pub async fn helper_db() -> Database {
+    let config = Settings::new().unwrap();
+    let config = config.database_config();
+    Database::new(config).await.unwrap()
+}
+
+pub async fn save_blocks(
+    db: &Database,
+    blocks: impl Iterator<Item = &mut Block>,
+    checksums: &HashMap<String, String>,
+) {
+    for block in blocks {
+        db.save_block(&block, &checksums).await.unwrap();
+    }
+}
