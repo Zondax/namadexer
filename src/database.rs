11720694e635eedf191c21b18fa5d7daa6092ca0
Emmanuel <emmanuelm41@gmail.com>
2023-09-13 15:40:33 -0300

    add missing if not exists clause for schema creation

--- src/database.rs
+++ src/database.rs
@@ -88,7 +88,7 @@ impl Database {
     pub async fn create_tables(&self) -> Result<(), Error> {
         info!("Creating tables if doesn't exist");
 
-        query(format!("CREATE SCHEMA {}", self.network).as_str())
+        query(format!("CREATE SCHEMA IF NOT EXISTS {}", self.network).as_str())
             .execute(&*self.pool)
             .await?;
 
