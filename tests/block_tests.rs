91b928978bddced04c0e52f369fcbba3f3c9fa97
sleepy ramen <rllola80@gmail.com>
2023-10-13 15:35:22 +0200

    update git workflow (#19)

--- tests/block_tests.rs
+++ tests/block_tests.rs
@@ -15,7 +15,7 @@ mod block_tests {
         // start server
         let cfg = Settings::new().unwrap();
 
-        let db = Database::new(cfg.database_config(), "public-testnet-12")
+        let db = Database::new(cfg.database_config(), "public-testnet-14")
             .await
             .unwrap();
 
@@ -51,7 +51,7 @@ mod block_tests {
         // start server
         let cfg = Settings::new().unwrap();
 
-        let db = Database::new(cfg.database_config(), "public-testnet-12")
+        let db = Database::new(cfg.database_config(), "public-testnet-14")
             .await
             .unwrap();
 
@@ -80,7 +80,7 @@ mod block_tests {
         // start server
         let cfg = Settings::new().unwrap();
 
-        let db = Database::new(cfg.database_config(), "public-testnet-12")
+        let db = Database::new(cfg.database_config(), "public-testnet-14")
             .await
             .unwrap();
 
@@ -90,7 +90,7 @@ mod block_tests {
         let address = format!("http://{}:{}", addr.ip(), addr.port());
         let hc = httpc_test::new_client(address).expect("Server not running?");
         let response = hc
-            .do_get("/block/hash/131f6c7727c64c785cd8b255ef35860fd021bff2a612c88e0ab19d05678021c6")
+            .do_get("/block/hash/2b719da000dd1caef4d401dbf9908610ce9059d052654887886957d8d1256d1a")
             .await
             .expect("Block does not exist");
 
@@ -98,6 +98,6 @@ mod block_tests {
 
         let header = response.json_body_as::<BlockInfo>().unwrap();
 
-        assert_eq!(header.tx_hashes.len(), 149)
+        assert_eq!(header.tx_hashes.len(), 197)
     }
 }
