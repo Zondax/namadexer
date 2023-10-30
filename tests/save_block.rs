61f82f8ecce0454cca9b1a598f045364f8b6458a
neithanmo <neithanmo@gmail.com>
2023-10-30 15:18:53 -0600

    Improve testing

--- tests/save_block.rs
+++ tests/save_block.rs
@@ -1,36 +1,33 @@
+mod utils;
+
 #[cfg(test)]
 mod save_block {
     use namadexer::utils::load_checksums;
-    use namadexer::Database;
-    use namadexer::Settings;
     use std::fs;
     use tendermint::block::Block;
 
+    use crate::utils::{create_test_db, destroy_test_db, helper_db, TESTING_DB_NAME};
+
     #[tokio::test]
-    async fn a_save_block() {
-        let config = Settings::new().unwrap();
-        let config = config.database_config();
+    async fn save_block() {
+        let helper_db = helper_db().await;
+
+        destroy_test_db(helper_db.pool(), TESTING_DB_NAME).await;
+
+        // now create a fresh database for tests
+        let db = create_test_db(helper_db.pool(), TESTING_DB_NAME).await;
 
         let checksums_map = load_checksums().unwrap();
 
         let data = fs::read_to_string("./tests/blocks_vector.json").unwrap();
         let blocks: Vec<Block> = serde_json::from_str(&data).unwrap();
 
-        let db = Database::new(config, "public-testnet-14").await.unwrap();
         db.create_tables().await.unwrap();
 
         for block in blocks {
             db.save_block(&block, &checksums_map).await.unwrap();
         }
-    }
 
-    #[tokio::test]
-    async fn b_create_indexes() {
-        let config = Settings::new().unwrap();
-        let config = config.database_config();
-
-        let db = Database::new(config, "public-testnet-14").await.unwrap();
-
-        db.create_indexes().await.unwrap();
+        assert!(db.create_indexes().await.is_ok());
     }
 }
--- tests/utils.rs
+++ tests/utils.rs
@@ -1,9 +1,12 @@
+use namadexer::{create_server, Database, Error as NError, ServerConfig, Settings};
+use sqlx::postgres::PgPoolOptions;
+use sqlx::query;
+use sqlx::PgPool;
 use std::net::SocketAddr;
+use std::time::Duration;
 
-use namadexer::create_server;
-use namadexer::Database;
-use namadexer::Error as NError;
-use namadexer::ServerConfig;
+pub const TESTING_DB_NAME: &str = "testingdb";
+const NETWORK: &str = "testnet";
 
 // start a server and return its address
 pub fn start_server(db: Database) -> Result<SocketAddr, NError> {
@@ -20,4 +23,56 @@ pub fn start_server(db: Database) -> Result<SocketAddr, NError> {
     tokio::spawn(server);
 
     Ok(socket)
+}
+
+async fn create_db(pool: &PgPool, name: &str) {
+    // now create bench database
+    let db_query = format!("CREATE DATABASE {}", name);
+
+    query(&db_query)
+        .execute(pool)
+        .await
+        .expect("Could not create database for benchmarks");
+}
+
+async fn destroy_db(pool: &PgPool, name: &str) {
+    let db_query = format!("DROP DATABASE {}", name);
+
+    _ = query(&db_query).execute(pool).await
+}
+
+pub async fn create_test_db(pg_pool: &PgPool, name: &str) -> Database {
+    create_db(pg_pool, name).await;
+
+    // now connect to the just created db
+    let mut config = Settings::new().unwrap();
+    config.database.dbname = name.to_string();
+
+    let config = config.database_config();
+
+    Database::new(config, NETWORK).await.unwrap()
+}
+
+pub async fn destroy_test_db(pg_pool: &PgPool, name: &str) {
+    destroy_db(pg_pool, name).await
+}
+
+// Returns a database that is used for creating the db used for testing,
+// here we connect to postgres database.
+pub async fn helper_db() -> Database {
+    let mut config = Settings::new().unwrap();
+    // Connect to default postgres database
+    config.database.dbname = "postgres".to_string();
+
+    let config = config.database_config();
+
+    Database::new(config, NETWORK).await.unwrap()
+}
+
+pub async fn testing_db() -> Database {
+    let mut config = Settings::new().unwrap();
+    config.database.dbname = TESTING_DB_NAME.to_string();
+
+    let config = config.database_config();
+    Database::new(config, NETWORK).await.unwrap()
 }
