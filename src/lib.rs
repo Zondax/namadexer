ab66dfe3fbee196ac8a4481ca4299129b7e173a3
rllola <rllola80@gmail.com>
2024-02-11 15:44:25 +0100

    rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -22,5 +22,4 @@ const DB_SAVE_TXS_DURATION: &str = "db_save_transactions_duration";
 const DB_SAVE_EVDS_DURATION: &str = "db_save_evidences_duration";
 const DB_SAVE_COMMIT_SIG_DURATION: &str = "db_save_commit_sig_duration";
 
-pub const MASP_ADDR: &str =
-    "tnam1pcqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqzmefah";
+pub const MASP_ADDR: &str = "tnam1pcqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqzmefah";
