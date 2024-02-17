b83c2ce4c9123ead9f146fe2652f5d485351bb00
msobh13 <78115394+msobh13@users.noreply.github.com>
2024-02-17 20:34:53 +0400

    Update tx.rs

--- src/server/tx.rs
+++ src/server/tx.rs
@@ -63,28 +63,21 @@ pub enum IbcTx {
 /// The relevant information regarding transactions and their types.
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
 pub struct TxInfo {
-    /// The hash that idenfities this transaction
     #[serde(with = "hex::serde")]
     hash: Vec<u8>,
-    /// The block this transaction belongs to.
     #[serde(with = "hex::serde")]
     block_id: Vec<u8>,
-    /// The transaction type encoded as a string
     tx_type: String,
-    /// id for the wrapper tx if the tx is decrypted. otherwise it is null.
     #[serde(with = "hex::serde")]
     wrapper_id: Vec<u8>,
-    /// The transaction fee only for tx_type Wrapper (otherwise empty)
     fee_amount_per_gas_unit: Option<String>,
     fee_token: Option<String>,
-    /// Gas limit (only for Wrapper tx)
     gas_limit_multiplier: Option<i64>,
-    /// The transaction code. Match what is in the checksum.js
     #[serde(serialize_with = "serialize_optional_hex")]
     code: Option<Vec<u8>>,
     #[serde(serialize_with = "serialize_optional_hex")]
     data: Option<Vec<u8>>,
-    /// Inner transaction type
+    return_code: Option<i32>, // New field for return_code
     tx: Option<TxDecoded>,
 }
 
@@ -189,9 +182,10 @@ impl TryFrom<Row> for TxInfo {
         let gas_limit_multiplier = row.try_get("gas_limit_multiplier")?;
         let code: Option<Vec<u8>> = row.try_get("code")?;
         let data: Option<Vec<u8>> = row.try_get("data")?;
+        let return_code = row.try_get("return_code").ok(); // Parsing return_code
 
         Ok(Self {
-            hash,
+             hash,
             block_id,
             tx_type,
             wrapper_id,
@@ -200,6 +194,7 @@ impl TryFrom<Row> for TxInfo {
             gas_limit_multiplier,
             code,
             data,
+            return_code, // Assigning return_code to the struct field
             tx: None,
         })
     }
