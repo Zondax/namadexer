3895f90fca69632dad1a53bea1ed202c37b6cb96
sleepy ramen <rllola80@gmail.com>
2024-03-11 13:22:37 +0100

    Update mod.rs
    fix the resolve conflict mess I made

--- src/indexer/mod.rs
+++ src/indexer/mod.rs
@@ -166,7 +166,7 @@ async fn get_block_results(
 fn blocks_stream<'a>(
     block: u64,
     chain_name: &'a str,
-    client: &HttpClient,
+    client: &'a HttpClient,
 ) -> impl Stream<Item = (Block, block_results::Response)> + 'a {
     futures::stream::iter(block..).then(move |i| async move {
         timeout(Duration::from_secs(30), get_block(i as u32, chain_name, client))
