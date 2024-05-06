148ff02e45df5f3fbf799d1be081414adb8e268a
opsecx <108930243+opsecx@users.noreply.github.com>
2024-05-06 23:38:30 +0200

    Add decoded memo field and only successful transactions (return_code = 0) to all views

--- src/views.rs
+++ src/views.rs
@@ -17,8 +17,9 @@ pub fn get_create_tx_become_validator_view_query(network: &str) -> String {
     data->>'description' AS description, 
     data->>'website' AS website, 
     data->>'discord_handle' AS discord_handle, 
-    data->>'avatar' AS avatar
-    FROM {network}.transactions WHERE code = '\\x5938c2b9962eb57bf309a604afc9aa6bc55851fd1791346c5c5795abaaa5f295';")
+    data->>'avatar' AS avatar,
+    convert_from(decode(TRIM(LEADING '\\x' FROM memo::varchar),'hex'), 'UTF8') as memo
+    FROM {network}.transactions WHERE code = '\\x5938c2b9962eb57bf309a604afc9aa6bc55851fd1791346c5c5795abaaa5f295' AND return_code = 0;")
 }
 
 pub fn get_drop_tx_bond_view_query(network: &str) -> String {
@@ -31,8 +32,9 @@ pub fn get_create_tx_bond_view_query(network: &str) -> String {
     hash AS txid,
     data->>'validator' AS validator,
     data->>'amount' AS amount,
-    data->>'source' AS source
-    FROM {network}.transactions WHERE code = '\\x40e55cdef50e0771eb2b3cfe78b841d988345113f7fbeaa8b158de04589bb9fc';")
+    data->>'source' AS source,
+    convert_from(decode(TRIM(LEADING '\\x' FROM memo::varchar),'hex'), 'UTF8') as memo
+    FROM {network}.transactions WHERE code = '\\x40e55cdef50e0771eb2b3cfe78b841d988345113f7fbeaa8b158de04589bb9fc' AND return_code = 0;")
 }
 
 pub fn get_drop_tx_bridge_pool_view_query(network: &str) -> String {
@@ -43,8 +45,9 @@ pub fn get_create_tx_bridge_pool_view_query(network: &str) -> String {
     format!("CREATE OR REPLACE VIEW {network}.tx_bridge_pool AS
     SELECT
     hash AS txid,
-    data
-    FROM {network}.transactions WHERE code = '\\x66412b1b01b6659dc196bef86bdb540181d90c2f984a13597f0aa2a4f9c9c907';")
+    data,
+    convert_from(decode(TRIM(LEADING '\\x' FROM memo::varchar),'hex'), 'UTF8') as memo
+    FROM {network}.transactions WHERE code = '\\x66412b1b01b6659dc196bef86bdb540181d90c2f984a13597f0aa2a4f9c9c907' AND return_code = 0;")
 }
 
 pub fn get_drop_tx_change_consensus_key_view_query(network: &str) -> String {
@@ -56,8 +59,9 @@ pub fn get_create_tx_change_consensus_key_view_query(network: &str) -> String {
     SELECT
     hash AS txid,
     data->>'validator' AS validator,
-    data->>'consensus_key' AS consensus_key
-    FROM {network}.transactions WHERE code = '\\x919be890123e9fbe7f85640811f3ca8edfebe94b7314118afbc84ff9c75ec488';")
+    data->>'consensus_key' AS consensus_key,
+    convert_from(decode(TRIM(LEADING '\\x' FROM memo::varchar),'hex'), 'UTF8') as memo
+    FROM {network}.transactions WHERE code = '\\x919be890123e9fbe7f85640811f3ca8edfebe94b7314118afbc84ff9c75ec488' AND return_code = 0;")
 }
 
 pub fn get_drop_tx_change_validator_comission_view_query(network: &str) -> String {
@@ -69,8 +73,9 @@ pub fn get_create_tx_change_validator_comission_view_query(network: &str) -> Str
     SELECT
     hash AS txid,
     data->>'validator' AS validator,
-    data->>'new_rate' AS new_rate
-    FROM {network}.transactions WHERE code = '\\xf76e025e52c75e34937e76fae43c6ed7544e2268e0bcdafeca0b05f9b7484b36';")
+    data->>'new_rate' AS new_rate,
+    convert_from(decode(TRIM(LEADING '\\x' FROM memo::varchar),'hex'), 'UTF8') as memo
+    FROM {network}.transactions WHERE code = '\\xf76e025e52c75e34937e76fae43c6ed7544e2268e0bcdafeca0b05f9b7484b36' AND return_code = 0;")
 }
 
 pub fn get_drop_tx_change_validator_metadata_view_query(network: &str) -> String {
@@ -87,8 +92,9 @@ pub fn get_create_tx_change_validator_metadata_view_query(network: &str) -> Stri
     data->>'website' AS website,
     data->>'discord_handle' AS discord_handle,
     data->>'avatar' AS avatar,
-    data->>'commission_rate' AS commission_rate
-    FROM {network}.transactions WHERE code = '\\x1f0981a2ff60b5e9619c5464839d8d4e08dac72a8185a21285ae7f0e9498dd8c';")
+    data->>'commission_rate' AS commission_rate,
+    convert_from(decode(TRIM(LEADING '\\x' FROM memo::varchar),'hex'), 'UTF8') as memo
+    FROM {network}.transactions WHERE code = '\\x1f0981a2ff60b5e9619c5464839d8d4e08dac72a8185a21285ae7f0e9498dd8c' AND return_code = 0;")
 }
 
 pub fn get_drop_tx_claim_rewards_view_query(network: &str) -> String {
@@ -100,8 +106,9 @@ pub fn get_create_tx_claim_rewards_view_query(network: &str) -> String {
     SELECT
     hash AS txid,
     data->>'validator' AS validator,
-    data->>'source' AS source
-    FROM {network}.transactions WHERE code = '\\x4af7ca07f6e6f2ad87ffe2c5fca90224544c45cc263e2e4d05775d782cac1f48';")
+    data->>'source' AS source,
+    convert_from(decode(TRIM(LEADING '\\x' FROM memo::varchar),'hex'), 'UTF8') as memo
+    FROM {network}.transactions WHERE code = '\\x4af7ca07f6e6f2ad87ffe2c5fca90224544c45cc263e2e4d05775d782cac1f48' AND return_code = 0;")
 }
 
 pub fn get_drop_tx_deactivate_validator_view_query(network: &str) -> String {
@@ -112,8 +119,9 @@ pub fn get_create_tx_deactivate_validator_view_query(network: &str) -> String {
     format!("CREATE OR REPLACE VIEW {network}.tx_deactivate_validator AS
     SELECT
     hash AS txid,
-    data AS address
-    FROM {network}.transactions WHERE code = '\\x0faaf9b55c150cdf8b2ea6a05c5fae725735b4fee44aa5da79bcd1881cb43f78';")
+    data AS address,
+    convert_from(decode(TRIM(LEADING '\\x' FROM memo::varchar),'hex'), 'UTF8') as memo
+    FROM {network}.transactions WHERE code = '\\x0faaf9b55c150cdf8b2ea6a05c5fae725735b4fee44aa5da79bcd1881cb43f78' AND return_code = 0;")
 }
 
 pub fn get_drop_tx_ibc_view_query(network: &str) -> String {
@@ -124,8 +132,9 @@ pub fn get_create_tx_ibc_view_query(network: &str) -> String {
     format!("CREATE OR REPLACE VIEW {network}.tx_ibc AS
     SELECT
     hash AS txid,
-    data
-    FROM {network}.transactions WHERE code = '\\xf99df82e284dcb96a12b409bc43aa7dc77b346ab0b2d3f0a9a39807e749ce8ee';")
+    data,
+    convert_from(decode(TRIM(LEADING '\\x' FROM memo::varchar),'hex'), 'UTF8') as memo
+    FROM {network}.transactions WHERE code = '\\xf99df82e284dcb96a12b409bc43aa7dc77b346ab0b2d3f0a9a39807e749ce8ee' AND return_code = 0;")
 }
 
 pub fn get_drop_tx_init_account_view_query(network: &str) -> String {
@@ -138,8 +147,9 @@ pub fn get_create_tx_init_account_view_query(network: &str) -> String {
     hash AS txid,
     data->>'public_keys' AS public_keys,
     data->>'vp_code_hash' AS vp_code_hash,
-    data->>'threshold' AS threshold
-    FROM {network}.transactions WHERE code = '\\x51e79a8f5d39b40db8531610398a8631e27390d48af80c916382779d7b0e7e41';")
+    data->>'threshold' AS threshold,
+    convert_from(decode(TRIM(LEADING '\\x' FROM memo::varchar),'hex'), 'UTF8') as memo
+    FROM {network}.transactions WHERE code = '\\x51e79a8f5d39b40db8531610398a8631e27390d48af80c916382779d7b0e7e41' AND return_code = 0;")
 }
 
 pub fn get_drop_tx_init_proposal_view_query(network: &str) -> String {
@@ -156,8 +166,9 @@ pub fn get_create_tx_init_proposal_view_query(network: &str) -> String {
     data->>'r#type' AS rtype,
     data->>'voting_start_epoch' AS voting_start_epoch,
     data->>'voting_end_epoch' AS voting_end_epoch,
-    data->>'grace_epoch' AS grace_epoch
-    FROM {network}.transactions WHERE code = '\\xb0a4e44eb0e8e3a1af49ebd2b0483260c67a17f974c7517904e88897279d1b29';")
+    data->>'grace_epoch' AS grace_epoch,
+    convert_from(decode(TRIM(LEADING '\\x' FROM memo::varchar),'hex'), 'UTF8') as memo
+    FROM {network}.transactions WHERE code = '\\xb0a4e44eb0e8e3a1af49ebd2b0483260c67a17f974c7517904e88897279d1b29' AND return_code = 0;")
 }
 
 pub fn get_drop_tx_reactivate_validator_view_query(network: &str) -> String {
@@ -168,8 +179,9 @@ pub fn get_create_tx_reactivate_validator_view_query(network: &str) -> String {
     format!("CREATE OR REPLACE VIEW {network}.tx_reactivate_validator AS
     SELECT
     hash AS txid,
-    data AS address
-    FROM {network}.transactions WHERE code = '\\xc94c4e6d549c921b9a483675f0e3af45eef79d4489bd35061fd285af6189b20d';")
+    data AS address,
+    convert_from(decode(TRIM(LEADING '\\x' FROM memo::varchar),'hex'), 'UTF8') as memo
+    FROM {network}.transactions WHERE code = '\\xc94c4e6d549c921b9a483675f0e3af45eef79d4489bd35061fd285af6189b20d' AND return_code = 0;")
 }
 
 pub fn get_drop_tx_redelegate_view_query(network: &str) -> String {
@@ -183,8 +195,9 @@ pub fn get_create_tx_redelegate_view_query(network: &str) -> String {
     data->>'redel_bond_start' AS redel_bond_start,
     data->>'src_validator' AS src_validator,
     data->>'bond_start' AS bond_start,
-    data->>'amount' AS amount
-    FROM {network}.transactions WHERE code = '\\x4afaa8c4ea6138a43d465ed09e86aedb16c54f63fd09a752a3aca5a26542e126';")
+    data->>'amount' AS amount,
+    convert_from(decode(TRIM(LEADING '\\x' FROM memo::varchar),'hex'), 'UTF8') as memo
+    FROM {network}.transactions WHERE code = '\\x4afaa8c4ea6138a43d465ed09e86aedb16c54f63fd09a752a3aca5a26542e126' AND return_code = 0;")
 }
 
 pub fn get_drop_tx_resign_steward_view_query(network: &str) -> String {
@@ -195,8 +208,9 @@ pub fn get_create_tx_resign_steward_view_query(network: &str) -> String {
     format!("CREATE OR REPLACE VIEW {network}.tx_resign_steward AS
     SELECT
     hash AS txid,
-    data AS address
-    FROM {network}.transactions WHERE code = '\\x7655ed64d1b07900672aee307f679b35af77337c077648adc11914348d1f130f';")
+    data AS address,
+    convert_from(decode(TRIM(LEADING '\\x' FROM memo::varchar),'hex'), 'UTF8') as memo
+    FROM {network}.transactions WHERE code = '\\x7655ed64d1b07900672aee307f679b35af77337c077648adc11914348d1f130f' AND return_code = 0;")
 }
 
 pub fn get_drop_tx_reveal_pk_view_query(network: &str) -> String {
@@ -207,8 +221,9 @@ pub fn get_create_tx_reveal_pk_view_query(network: &str) -> String {
     format!("CREATE OR REPLACE VIEW {network}.tx_reveal_pk AS
     SELECT
     hash AS txid,
-    data AS public_key
-    FROM {network}.transactions WHERE code = '\\x283fd236d971dd0f7ca1a329b508a4039946f40f1c9792863fe6b0fa05d74832';")
+    data AS public_key,
+    convert_from(decode(TRIM(LEADING '\\x' FROM memo::varchar),'hex'), 'UTF8') as memo
+    FROM {network}.transactions WHERE code = '\\x283fd236d971dd0f7ca1a329b508a4039946f40f1c9792863fe6b0fa05d74832' AND return_code = 0;")
 }
 
 pub fn get_drop_tx_transfert_view_query(network: &str) -> String {
@@ -228,7 +243,8 @@ pub fn get_create_tx_transfer_view_query(network: &str) -> String {
     data->>'token' AS token,
     data->>'amount' AS amount,
     data->>'key' AS key,
-    data->>'shielded' AS shielded
+    data->>'shielded' AS shielded,
+    convert_from(decode(TRIM(LEADING '\\x' FROM memo::varchar),'hex'), 'UTF8') as memo
     FROM {network}.transactions WHERE code = '\\x0960374d23acbac1feb27b3888095859217936c900cef54e559d215cec3206ef' AND return_code = 0;")
 }
 
@@ -242,8 +258,9 @@ pub fn get_create_tx_unbond_view_query(network: &str) -> String {
     hash AS txid,
     data->>'validator' AS validator,
     data->>'amount' AS amount,
-    data->>'source' AS source
-    FROM {network}.transactions WHERE code = '\\xe39415d64bdc3c16f9b21ebee4e9496f8ce5cdd5551d1e611c449d8bfdcffae0';")
+    data->>'source' AS source,
+    convert_from(decode(TRIM(LEADING '\\x' FROM memo::varchar),'hex'), 'UTF8') as memo
+    FROM {network}.transactions WHERE code = '\\xe39415d64bdc3c16f9b21ebee4e9496f8ce5cdd5551d1e611c449d8bfdcffae0' AND return_code = 0;")
 }
 
 pub fn get_drop_tx_unjail_validator_view_query(network: &str) -> String {
@@ -254,8 +271,9 @@ pub fn get_create_tx_unjail_validator_view_query(network: &str) -> String {
     format!("CREATE OR REPLACE VIEW {network}.tx_unjail_validator AS
     SELECT
     hash AS txid,
-    data AS address
-    FROM {network}.transactions WHERE code = '\\x2b1451721dcdd069a19cba1f9b338bb6a45d85d0d56ba7ca952742d3ec5878b3';")
+    data AS address,
+    convert_from(decode(TRIM(LEADING '\\x' FROM memo::varchar),'hex'), 'UTF8') as memo
+    FROM {network}.transactions WHERE code = '\\x2b1451721dcdd069a19cba1f9b338bb6a45d85d0d56ba7ca952742d3ec5878b3' AND return_code = 0;")
 }
 
 pub fn get_drop_tx_update_account_view_query(network: &str) -> String {
@@ -269,8 +287,9 @@ pub fn get_create_tx_update_account_view_query(network: &str) -> String {
     data->>'addr' AS addr,
     data->>'vp_code_hash' AS vp_code_hash,
     data->>'public_keys' AS public_keys,
-    data->>'threshold' AS threshold
-    FROM {network}.transactions WHERE code = '\\x70f91d4f778d05d40c5a56490ced906b016e4b7a2a2ef5ff0ac0541ff28c5a22';")
+    data->>'threshold' AS threshold,
+    convert_from(decode(TRIM(LEADING '\\x' FROM memo::varchar),'hex'), 'UTF8') as memo
+    FROM {network}.transactions WHERE code = '\\x70f91d4f778d05d40c5a56490ced906b016e4b7a2a2ef5ff0ac0541ff28c5a22' AND return_code = 0;")
 }
 
 pub fn get_drop_tx_update_steward_commission_view_query(network: &str) -> String {
@@ -282,8 +301,9 @@ pub fn get_create_tx_update_steward_commission_view_query(network: &str) -> Stri
     SELECT
     hash AS txid,
     data->>'steward' AS steward,
-    data->>'commission' AS commission 
-    FROM {network}.transactions WHERE code = '\\xed0ccfa4a8c8fa86f9f14c3b53b7f126ce4a86849815dad784b5aa35011a1db6';")
+    data->>'commission' AS commission,
+    convert_from(decode(TRIM(LEADING '\\x' FROM memo::varchar),'hex'), 'UTF8') as memo
+    FROM {network}.transactions WHERE code = '\\xed0ccfa4a8c8fa86f9f14c3b53b7f126ce4a86849815dad784b5aa35011a1db6' AND return_code = 0;")
 }
 
 pub fn get_drop_tx_vote_proposal_view_query(network: &str) -> String {
@@ -297,7 +317,8 @@ pub fn get_create_tx_vote_proposal_view_query(network: &str) -> String {
     data->>'id' AS id,
     data->>'vote' AS vote,
     data->>'voter' AS voter,
-    data->>'delegations' AS delegations
+    data->>'delegations' AS delegations,
+    convert_from(decode(TRIM(LEADING '\\x' FROM memo::varchar),'hex'), 'UTF8') as memo
     FROM {network}.transactions WHERE code = '\\xccdbe81f664ca6c2caa11426927093dc10ed95e75b3f2f45bffd8514fee47cd0' AND return_code = 0;")
 }
 
@@ -310,6 +331,7 @@ pub fn get_create_tx_withdraw_view_query(network: &str) -> String {
     SELECT
     hash AS txid,
     data->'validator' AS validator,
-    data->'source' AS source
-    FROM {network}.transactions WHERE code = '\\x69560777e2656b2872a49080c51bb5b1a498a7ffd79dae491d36b301a1b012e6';")
+    data->'source' AS source,
+    convert_from(decode(TRIM(LEADING '\\x' FROM memo::varchar),'hex'), 'UTF8') as memo
+    FROM {network}.transactions WHERE code = '\\x69560777e2656b2872a49080c51bb5b1a498a7ffd79dae491d36b301a1b012e6' AND return_code = 0;")
 }
